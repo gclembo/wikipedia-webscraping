c415e5ac82a9b84b59c7dabff1d7eb1ba5081811
gclembo <gcarterlembo@gmail.com>
2025-07-01 14:06:04 -0700

    added search feature and updated structure

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import sqlite3, json, pickle
 from pathlib import Path
-from flask import Flask, render_template
+
+from flask import Flask, request, render_template
 from search_engine.word_calculations import Page, SearchEngine
 
 file_path = Path(__file__)
@@ -19,17 +20,23 @@ def load_data():
 
 wiki_data = load_data()
 
+with open(data_folder / "search_engine.pickle", "rb") as file:
+    wiki_engine = pickle.load(file)
+
 app = Flask(__name__)
+
 
 def make_data_list(col_name):
     return json.dumps(wiki_data[col_name].tolist())
 
-
-with open(data_folder / "search_engine.pickle", "rb") as file:
-    wiki_engine = pickle.load(file)
-
-
+# index
 @app.route("/")
+def run_index():
+    return render_template("index.html")
+
+
+# dashboard
+@app.route("/dashboard")
 def run_dashboard():
     return render_template(
         "dashboard.html",
@@ -40,6 +47,17 @@ def run_dashboard():
         published=make_data_list("published"),
         modified=make_data_list("modified")
     )
+
+# search engine
+@app.route("/search", methods=["GET", "POST"])
+def run_search():
+    pages_found = []
+    if request.method == "POST":
+        query = request.form.get("query")
+        pages_found = wiki_engine.search(query)
+
+    pages_found = json.dumps(pages_found)
+    return render_template("search_engine.html", pages_found=pages_found)
 
 if __name__ == "__main__":
     app.run(debug=True)
